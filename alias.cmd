3a67a7e13bd3917e45440955273cbbfcf9fb06b3
Antonis Pappas <48538391+hypercurious@users.noreply.github.com>
2021-04-19 22:07:20 +0300

    Update alias.cmd

--- alias.cmd
+++ alias.cmd
@@ -1,7 +1,7 @@
-title Terminal & prompt antonis@vantablack[$T]$H$H$H$H]$G
-cd C:\Users\Vantablack\Desktop
+title Terminal & prompt username@pc-name[$T]$H$H$H$H]$G
+cd %userprofile%\Desktop
 DOSKEY ll=ls -la
 DOSKEY ceid=cd "%userprofile%\Desktop\CEID"
-DOSKEY diogenis=ssh papa@diogenis.ceid.upatras.gr
+DOSKEY diogenis=ssh username@diogenis.ceid.upatras.gr
 DOSKEY alias=vim %userprofile%\alias.cmd
 cls
