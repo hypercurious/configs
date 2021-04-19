b5a5194aecc596b08655ac7d9154340bf44778d1
hypercurious <48538391+hypercurious@users.noreply.github.com>
2021-04-19 22:07:53 +0300

    Update alias.cmd

--- alias.cmd
+++ alias.cmd
@@ -1,7 +1,7 @@
 title Terminal & prompt username@pc-name[$T]$H$H$H$H]$G
 cd %userprofile%\Desktop
 DOSKEY ll=ls -la
-DOSKEY ceid=cd "%userprofile%\Desktop\CEID"
+DOSKEY ceid=cd %userprofile%\Desktop\CEID
 DOSKEY diogenis=ssh username@diogenis.ceid.upatras.gr
 DOSKEY alias=vim %userprofile%\alias.cmd
 cls
