8bbbfd7eb17a2ab37a7afc3a548940e423e456e7
hypercurious <48538391+hypercurious@users.noreply.github.com>
2021-04-19 22:03:55 +0300

    windows cmd configs

---
+++ alias.cmd
@@ -0,0 +1,7 @@
+title Terminal & prompt antonis@vantablack[$T]$H$H$H$H]$G
+cd C:\Users\Vantablack\Desktop
+DOSKEY ll=ls -la
+DOSKEY ceid=cd "%userprofile%\Desktop\CEID"
+DOSKEY diogenis=ssh papa@diogenis.ceid.upatras.gr
+DOSKEY alias=vim %userprofile%\alias.cmd
+cls
