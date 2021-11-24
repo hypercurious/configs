8b66220d5a7e1985724df3d6c105b88177c6642e
Anton Papa <48538391+hypercurious@users.noreply.github.com>
2021-11-24 17:50:07 +0200

    Update alias.cmd

--- alias.cmd
+++ alias.cmd
@@ -1,7 +1,17 @@
-title Terminal & prompt username@pc-name[$T]$H$H$H$H]$G
+@echo off
+
+title Terminal
+prompt hypercurious@%USERNAME%[$T]$H$H$H$H]$G
 cd %userprofile%\Desktop
+
 DOSKEY ll=ls -la
+DOSKEY py=python $1
 DOSKEY ceid=cd %userprofile%\Desktop\CEID
 DOSKEY diogenis=ssh username@diogenis.ceid.upatras.gr
 DOSKEY alias=vim %userprofile%\alias.cmd
+DOSKEY rc=vim %userprofile%\.vimrc
+DOSKEY sd=shutdown -s -t $1
+DOSKEY abort=shutdown -a
+DOSKEY clear=cls
+
 cls
